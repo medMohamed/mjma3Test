0848e628cf9155d3e498cae55c23fd2088b892ea
simohamed <mohamed.sakhri@gmail.com>
2012-10-12 16:35:05 +0200

    first test

---
+++ myFirstProject/MyObject.cpp
@@ -0,0 +1,20 @@
+/*
+ * MyObject.cpp
+ *
+ *  Created on: Oct 11, 2012
+ *      Author: simohamed
+ */
+
+#include "MyObject.h"
+#include <iostream>
+
+using namespace std;
+
+MyObject::MyObject() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MyObject::~MyObject() {
+	// TODO Auto-generated destructor stub
+}
---
+++ myFirstProject/MyObject.h
@@ -0,0 +1,20 @@
+/*
+ * MyObject.h
+ *
+ *  Created on: Oct 11, 2012
+ *      Author: simohamed
+ */
+
+#ifndef MYOBJECT_H_
+#define MYOBJECT_H_
+
+class MyObject {
+public:
+	MyObject(); // constructor
+	virtual ~MyObject(); // destructor
+
+private :
+	int myPrivate;
+};
+
+#endif /* MYOBJECT_H_ */
